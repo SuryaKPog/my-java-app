60709bf322708d7ef07f7dc487ee941542f1461f
Surya KP <127583162+SuryaKPog@users.noreply.github.com>
2025-10-22 11:52:11 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,5 +3,17 @@ output "eks_cluster_endpoint" {
 }
 
 output "eks_cluster_name" {
-  value = module.eks.cluster_id
+  value = module.eks.cluster_name
+}
+
+output "cluster_security_group_id" {
+  value = aws_security_group.eks_sg.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
 }
